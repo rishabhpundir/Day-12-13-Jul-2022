b43af82a88bd454c7c81c56fb49d3ccfe0f17066
rishabhpundir <th3rishabh@gmail.com>
2022-07-13 18:32:02 +0530

    Day 12 13 Jul 2022

---
+++ day12_star_patterns.py
@@ -0,0 +1,146 @@
+# 1. RIGHT ANGLE TRIANGLE (LEFT LEAN)
+
+#  * 
+#  * *
+#  * * *
+#  * * * *
+#  * * * * *
+
+# star = '*'
+# space = ' '
+# j_range = 0
+# for i in range(5):
+#     j_range+=1
+#     star_len = ''
+#     for j in range(j_range):
+#         star_len = star_len+star+space
+#     print(star_len)
+        
+
+# ---------------------------------------------------------------------------------------
+
+
+# 2. RIGHT ANGLE TRIANGLE (RIGHT LEAN)
+
+#          *
+#        * *
+#      * * *
+#    * * * *
+#  * * * * *
+
+# star = '*'
+# space = ' '
+# j_range = 0
+# k_range = 5
+# for i in range(5):
+#     j_range+=1
+#     k_range-=1
+#     star_len = ''
+#     for k in range(k_range):
+#             star_len = star_len+space+space
+#     for j in range(j_range):
+#         star_len = star_len+space+star
+#     print(star_len)
+
+
+# ---------------------------------------------------------------------------------------
+
+
+# 3. Triangle
+
+#     * 
+#    * *
+#   * * *
+#  * * * *
+# * * * * *
+
+# star = '*'
+# space = ' '
+# j_range = 0
+# k_range = 5
+# for i in range(5):
+#     j_range+=1
+#     k_range-=1
+#     star_len = ''
+#     for k in range(k_range):
+#             star_len = star_len+space
+#     for j in range(j_range):
+#         star_len = star_len+star+space
+#     print(star_len)
+
+
+# ---------------------------------------------------------------------------------------
+
+
+# 4. Tilted Triangle
+
+#  * * * * * 
+#   * * * *
+#    * * *
+#     * *
+#      *
+
+# star = '*'
+# space = ' '
+# j_range = 6
+# k_range = 0
+# for i in range(6):
+#     j_range-=1
+#     k_range+=1
+#     star_len = ''
+#     for k in range(k_range):
+#             star_len = star_len+space
+#     for j in range(j_range):
+#         star_len = star_len+star+space
+#     print(star_len)
+
+
+# ---------------------------------------------------------------------------------------
+
+
+# 5. TILTED RIGHT ANGLE TRIANGLE (RIGHT LEAN)
+
+#    * * * * *
+#      * * * *
+#        * * *
+#          * *
+#            *
+
+# star = '*'
+# space = ' '
+# j_range = 6
+# k_range = 0
+# for i in range(6):
+#     j_range-=1
+#     k_range+=1
+#     star_len = ''
+#     for k in range(k_range):
+#             star_len = star_len+space+space
+#     for j in range(j_range):
+#         star_len = star_len+space+star
+#     print(star_len)
+
+
+# ---------------------------------------------------------------------------------------
+
+
+# 6. TILTED RIGHT ANGLE TRIANGLE (LEFT LEAN)
+
+# * * * * * 
+# * * * *
+# * * *
+# * *
+# *
+
+# star = '*'
+# space = ' '
+# j_range = 6
+# for i in range(6):
+#     j_range-=1
+#     star_len = ''
+#     for j in range(j_range):
+#         star_len = star_len+star+space
+#     print(star_len)
+
+
+# ---------------------------------------------------------------------------------------
